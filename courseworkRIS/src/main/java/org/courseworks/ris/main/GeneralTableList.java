d2f243b1c690880f7499cadb994a654583d95ed8
Deim <Deim@localhost>
2009-11-27 20:44:41 +0300

    [coursework-ris] +mappings.hprepair.DatabaseLowerWorker

--- courseworkRIS/src/main/java/org/courseworks/ris/main/GeneralTableList.java
+++ courseworkRIS/src/main/java/org/courseworks/ris/main/GeneralTableList.java
@@ -3,17 +3,17 @@ package org.courseworks.ris.main;
 import org.courseworks.ris.cmanager.ConnectionsManager;
 import org.courseworks.ris.cmanager.session.ExtendedSession;
 import org.courseworks.ris.cmanager.session.TableList;
+import org.courseworks.ris.mappings.hprepair.DatabaseLowerWorker;
 import org.courseworks.ris.widgets.viewers.DbTable;
 
 public class GeneralTableList extends TableList {
 
 	@Override
 	protected void fillTables() {
-		DbTable cars = new DbTable("Cars");
-		DbTable drivers = new DbTable("Drivers");
-		getTables().add(cars);
-		getTables().add(drivers);
-		
+		for (String tableName : DatabaseLowerWorker.getEntities()) {
+			getTables().add(new DbTable(tableName));
+		}
+
 		for (String sessionKey : ConnectionsManager.getSessions()) {
 			ExtendedSession currentSession = ConnectionsManager
 					.getSession(sessionKey);
---
+++ courseworkRIS/src/main/java/org/courseworks/ris/mappings/hprepair/DatabaseLowerWorker.java
@@ -0,0 +1,26 @@
+package org.courseworks.ris.mappings.hprepair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.courseworks.ris.main.AbstractEntity;
+import org.hibernate.Session;
+
+public class DatabaseLowerWorker {
+	public static String CARS = "Cars", DRIVERS = "Drivers";
+
+	public static List<AbstractEntity> getEntity(Session session, String table) {
+		List<AbstractEntity> forReturn = new ArrayList<AbstractEntity>();
+		for (Object obj : session.createQuery("from " + table).list()) {
+			forReturn.add((AbstractEntity) obj);
+		}
+		return forReturn;
+	}
+
+	public static List<String> getEntities() {
+		List<String> forReturn = new ArrayList<String>();
+		forReturn.add("Cars");
+		forReturn.add("Drivers");
+		return forReturn;
+	}
+}
