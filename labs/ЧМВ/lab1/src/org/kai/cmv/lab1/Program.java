67dbd5b8d70ab11e2de3b610afa87158bd40f2d6
aidarbariev <aidarbariev@localhost>
2011-09-12 00:37:49 +0400

    ???????? push(4)

--- labs/ЧМВ/lab1/src/org/kai/cmv/lab1/Program.java
+++ labs/ЧМВ/lab1/src/org/kai/cmv/lab1/Program.java
@@ -15,7 +15,7 @@ public class Program {
 		final Shell shell = new Shell(display);
 		shell.setLayout(new FillLayout());
 
-		System.out.println("qwe");
+		System.out.println("qqwe");
 		new NumbersView(shell, SWT.NONE);
 
 		shell.setSize(shellSize);
