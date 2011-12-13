d11f4e21759bcaa3e38990c222834e40ba87fc80
Deim <Deim@localhost>
2011-12-13 19:02:38 +0300

    [4MV-lab4] ??????, ???? ????? ????

--- labs/4MB/lab4/src/main/java/org/kai/CMV/lab4/main/Application.java
+++ labs/4MB/lab4/src/main/java/org/kai/CMV/lab4/main/Application.java
@@ -1,52 +1,26 @@
 ﻿package org.kai.CMV.lab4.main;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.Locale;
-
-import javassist.NotFoundException;
-
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-import org.kai.CMV.lab4.cmanager.ConfigurationsManager;
-import org.kai.CMV.lab4.cmanager.SessionsManager;
-import org.kai.CMV.lab4.cmanager.session.ExtendedSession;
-import org.kai.CMV.lab4.cmanager.session.GeneralSession;
 import org.kai.CMV.lab4.gui.ProgWin;
 
 public class Application {
-
-	public static final String IMAGE = "/images/gnumeric-splash-screen.png";
-
 	private static Shell _shell;
 
-	private static SplashScreen _splash;
-
-	private static GeneralSession _generalSession;
-
 	public static void main(String[] args) {
-		System.out.println("CLIENT LOCALE ----------"
-				+ System.getenv("CLIENT_LOCALE"));
-
-		_splash = new SplashScreen();
-		_splash.setRunnable(new InfromixDBInitialization());
-		_splash.open();
-
 		try {
 			initGUI();
 		} catch (IllegalArgumentException e) {
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
 			e.printStackTrace();
-		} catch (NotFoundException e) {
-			e.printStackTrace();
 		}
 	}
 
 	private static void initGUI() throws IllegalArgumentException,
-			IllegalAccessException, NotFoundException {
+			IllegalAccessException {
 		final Point shellSize = new Point(1000, 600);
 		Display display = new Display();
 
@@ -54,7 +28,7 @@ public class Application {
 		_shell.setLayout(new GridLayout());
 		_shell.setText(SC.APP_HEADER);
 
-		new ProgWin(_generalSession, _shell);
+		new ProgWin(_shell);
 
 		_shell.setSize(shellSize);
 		_shell.open();
@@ -66,86 +40,4 @@ public class Application {
 		}
 		display.dispose();
 	}
-
-	static class InfromixDBInitialization implements Runnable {
-
-		/**
-		 * {@inheritDoc}
-		 */
-		@Override
-		public void run() {
-			try {
-
-				Locale.setDefault(Locale.getAvailableLocales()[107]);
-				_splash.setSelection(3);
-
-				String path = "/informix_servers";
-
-				SessionsManager.initType(SessionsManager.ORGELQUEUE_SESSION);
-				SessionsManager.createSessions(new ConfigurationsManager(path));
-
-				_splash.setSelection(7);
-
-				fillingSessionsTables();
-
-				_splash.setSelection(10);
-
-				_generalSession = SessionsManager.getGeneralSession();
-
-				_splash.setSelection(15);
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (URISyntaxException e) {
-				e.printStackTrace();
-			}
-
-		}
-
-	}
-
-	static class MSSqlDBInitialization implements Runnable {
-
-		/**
-		 * {@inheritDoc}
-		 */
-		@Override
-		public void run() {
-			try {
-				_splash.setSelection(3);
-
-				String path = "/mssql_servers";
-				SessionsManager.initType(SessionsManager.HPREPAIR_SESSION);
-				SessionsManager.createSessions(new ConfigurationsManager(path));
-
-				_splash.setSelection(7);
-
-				fillingSessionsTables();
-
-				_splash.setSelection(10);
-
-				_generalSession = SessionsManager.getGeneralSession();
-
-				_splash.setSelection(15);
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (URISyntaxException e) {
-				e.printStackTrace();
-			}
-
-		}
-
-	}
-
-	public static void fillingSessionsTables() {
-		for (String sessionKey : SessionsManager.getSessionsNames()) {
-			ExtendedSession currentSession = SessionsManager
-					.getSession(sessionKey);
-			currentSession.fillTables();
-		}
-	}
-
-	public static GeneralSession getGenTables() {
-		return _generalSession;
-	}
-
 }
--- labs/4MB/lab4/src/main/java/org/kai/CMV/lab4/widgets/ExtendedTable.java
+++ labs/4MB/lab4/src/main/java/org/kai/CMV/lab4/widgets/ExtendedTable.java
@@ -1,106 +1,84 @@
 package org.kai.CMV.lab4.widgets;
 
-import java.lang.reflect.Field;
+import java.util.List;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
-import org.kai.CMV.lab4.cmanager.SessionsManager;
-import org.kai.CMV.lab4.cmanager.session.EntitySet;
-import org.kai.CMV.lab4.mappings.AbstractEntity;
 
 public class ExtendedTable extends Table {
+	public static int TABLE1 = 1, TABLE2 = 2;
+	private static List<Table> _tables;
+	private static int _currentTable = -1;
 
-    private EntitySet _dbTable;
-    private Field[] _fields;
+	public ExtendedTable(Composite parent, int style) {
+		super(parent, style);
+	}
 
-    public ExtendedTable(Composite parent, int style) {
-        super(parent, style);
-    }
+	public void fill(int table) throws IllegalArgumentException,
+			IllegalAccessException {
+		_currentTable = table;
+		refresh();
+	}
 
-    public void initType(EntitySet dbTable) {
-        removeAll();
-        for (TableColumn column : getColumns()) {
-            column.dispose();
-        }
+	public void refresh() throws IllegalArgumentException,
+			IllegalAccessException {
+		removeAll();
+		if (_currentTable != -1) {
+			for (TableItem item : _tables.get(_currentTable).getItems()) {
+				TableItem newRow = new TableItem(this, SWT.NONE);
+				newRow.setData(item);
+			}
+			for (int i = 0; i < _tables.get(_currentTable).getColumnCount(); i++) {
+				this.getColumn(i).setText(
+						_tables.get(_currentTable).getColumns()[i].getText());
+			}
+		}
+	}
 
-        _fields = dbTable.getViewableFields();
-        for (Field fld : _fields) {
-            TableColumn newColumn = new TableColumn(this, SWT.NONE);
-            newColumn.setData(fld);
-            newColumn.setText(dbTable.getFieldPresentation(fld));
-        }
+	public static List<Class<?>> getFields() {
+		List<Class<?>> outList = null;
+		switch (_currentTable) {
+		case 1:
+			break;
+		case 2:
+			break;
+		}
+		return outList;
+	}
 
-        TableColumn sessionColumn = new TableColumn(this, SWT.NONE);
-        sessionColumn.setText("Сервер");
-    }
+	public static List<String> getHeaders() {
+		List<String> columnHeaders = null;
+		for (TableColumn column : _tables.get(_currentTable).getColumns()) {
+			columnHeaders.add(column.getText());
+		}
+		return columnHeaders;
+	}
 
-    public void fill(EntitySet dbTable) throws IllegalArgumentException,
-            IllegalAccessException {
-        _dbTable = dbTable;
-        refresh();
-    }
+	@Override
+	public void pack() {
+		TableColumn[] columns = getColumns();
 
-    public void refresh() throws IllegalArgumentException,
-            IllegalAccessException {
-        removeAll();
-        if (_dbTable != null) {
-            for (AbstractEntity obj : _dbTable.getItems()) {
-                TableItem newRow = new TableItem(this, SWT.NONE);
-                newRow.setData(obj);
+		int columnsWidth = 0;
+		for (TableColumn column : columns) {
+			column.pack();
+			columnsWidth += column.getWidth();
+		}
 
-                int index = 0;
-                for (Field fld : _fields) {
-                    Object value = obj.getFieldValue(fld);
-                    if (value != null) {
-                        newRow.setText(index, value.toString());
-                    }
-                    index++;
-                }
-                newRow.setText(index,
-                        SessionsManager.getName(obj.getTable().getSession()));
-            }
-        }
-    }
+		// Если колонки суммарно имеют меньшюю ширину, чем таблица
+		int difference = getSize().x - columnsWidth;
+		if (difference > 0) {
+			int additionalWidth = difference / columns.length;
+			for (TableColumn column : columns) {
+				column.setWidth(column.getWidth() + additionalWidth);
+			}
+		}
+	}
 
-    public AbstractEntity getSelectedItem() {
-        int index = getSelectionIndex();
+	@Override
+	public void checkSubclass() {
 
-        if (index == -1) {
-            return null;
-        }
-
-        return (AbstractEntity) getItem(index).getData();
-    }
-
-    public EntitySet getTable() {
-        return _dbTable;
-    }
-
-    @Override
-    public void pack() {
-        TableColumn[] columns = getColumns();
-
-        int columnsWidth = 0;
-        for (TableColumn column : columns) {
-            column.pack();
-            columnsWidth += column.getWidth();
-        }
-
-        // Если колонки суммарно имеют меньшюю ширину, чем таблица
-        int difference = getSize().x - columnsWidth;
-        if (difference > 0) {
-            int additionalWidth = difference / columns.length;
-            for (TableColumn column : columns) {
-                column.setWidth(column.getWidth() + additionalWidth);
-            }
-        }
-    }
-
-    @Override
-    public void checkSubclass() {
-
-    }
+	}
 }
