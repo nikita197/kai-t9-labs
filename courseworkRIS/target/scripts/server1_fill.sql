7ca9f646952f3ac294b7701c7ed55cdb108ec23d
aidarbariev <aidarbariev@localhost>
2011-12-13 20:19:24 +0300

    [4MB] ?????? ? ??????1

---
+++ courseworkRIS/target/scripts/server1_fill.sql
@@ -0,0 +1,32 @@
+INSERT INTO markhelper VALUES ("BMW-X5", "BMW X5");
+INSERT INTO markhelper VALUES ("BMW-760", "BMW 760");
+INSERT INTO markhelper VALUES ("LEXUS-ES", "Lexus ES");
+INSERT INTO markhelper VALUES ("LEXUS-GS", "Lexus GS");
+INSERT INTO markhelper VALUES ("VOLVO-S40", "Volvo S40");
+INSERT INTO markhelper VALUES ("VOLVO-C70","Volvo C70");
+INSERT INTO markhelper VALUES ("AUDI-Q7","Audi Q7");
+INSERT INTO markhelper VALUES ("AUDI-A8","Audi A8");
+INSERT INTO markhelper VALUES ("VAZ-2109","Ваз 2109");
+INSERT INTO markhelper VALUES ("VAZ-2110","Ваз 2110");
+INSERT INTO markhelper VALUES ("VAZ-2112","Ваз 2112");
+INSERT INTO markhelper VALUES ("VAZ-2114","Ваз 2114");
+
+INSERT INTO car VALUES (200100,"BMW-X5","B145HP","116rus");
+INSERT INTO car VALUES (200101,"BMW-760","C345HP","116rus");
+INSERT INTO car VALUES (200102,"LEXUS-ES","K748HP","116rus");
+
+INSERT INTO client VALUES(300104,"Демидов","Сергей","Владимирович","7245 512341");
+INSERT INTO client VALUES(300105,"Киселев","Алексей","Владимирович","1242 612344");
+INSERT INTO client VALUES(300106,"Павлова","Татьяна","Витальевна","4512 925612");
+INSERT INTO client VALUES(300107,"Хромова","Ирина","Анатольевна","7223 034561");
+
+INSERT INTO parking_place VALUES(400103,"4J","f","Место 4J");
+INSERT INTO parking_place VALUES(400104,"4H","f","Место 4H");
+INSERT INTO parking_place VALUES(400105,"BN","f","Место BN");
+INSERT INTO parking_place VALUES(400106,"4T","f","Место 4T");
+INSERT INTO parking_place VALUES(400107,"8H","f","Место 8H");
+
+INSERT INTO rate VALUES (500100,50);
+
+INSERT INTO parking VALUES (600101,300104,200101,400106,"2010-10-17 08:23:54","");
+
